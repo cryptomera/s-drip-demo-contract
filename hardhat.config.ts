210d38f3b3fb2607114d844c4e90d7d7baf97e77
CodeWarrier <CodeWarrier3000@gmail.com>
2022-02-23 18:31:23 +0900

    drip token

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,7 +24,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4"
+      },
+      {
+        version: "0.4.25",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ],
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",

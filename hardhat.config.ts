109f31433da43ebe609cffc8a4be1b6318d9833d
CodeWarrier <CodeWarrier3000@gmail.com>
2022-03-11 02:22:38 +0900

    updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,6 +30,18 @@ const config: HardhatUserConfig = {
         version: "0.8.4"
       },
       {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.6.6"
+      },
+      {
         version: "0.4.25",
         settings: {
           optimizer: {
